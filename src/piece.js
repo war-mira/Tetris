b2fc7dd976f49d8e9bdce0704cd75793bf5297c8
war-mira <tvixa40@gmail.com>
2020-02-06 23:09:23 +0600

    -create project

---
+++ src/piece.js
@@ -0,0 +1,36 @@
+class Piece{
+    x;
+    y; 
+    color;
+    shape;
+    ctx;
+
+    constructor (ctx){
+        this.ctx = ctx
+        this.spawn()
+    }
+
+    spawn(){
+        this.color = 'blue'
+        this.shape = [
+            [2, 0, 0],
+            [2, 2, 2],
+            [0, 0, 0]
+        ]
+
+        this.x = 3
+        this.y = 3
+
+    }
+
+    draw(){
+        this.ctx.fillStyle = this.color
+        this.shape.forEach((row, y) => {
+            row.forEach((value, x) =>{
+                if(value>0){
+                    this.ctx.fillRect(this.x + x, this.y+y, 1, 1)
+                }
+            })
+        });
+    }
+}
